699dd035247352be9eaea0a598d6425bb42ca136
m7med.magdi <m7med.magdi@gmail.com>
2022-11-09 19:49:20 +0200

    Custom Toast Using Kotlin

--- app/src/main/java/com/m7med/customtoast/MainActivity.kt
+++ app/src/main/java/com/m7med/customtoast/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.m7med.customtoast
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.m7med.customtoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
         setContentView(R.layout.activity_main)
+        binding.btnunlike.setOnClickListener {
+
+            val toast: Toast = Toast.makeText(this, "Like Toast", Toast.LENGTH_LONG) as Toast
+
+
+        }
     }
 }
